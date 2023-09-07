9487188a38f353127ba6141874a784a7d6ad7291
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-07 14:32:57 +0300

    первое задание готово и мне надо идти

--- script.js
+++ script.js
@@ -18,3 +18,12 @@
     console.log('Наименование продукта: ', nameProduct);
     console.log('Общая сумма товара: ', quantityProduct*countProduct, 'рублей');
 }
+
+{
+    const nameProduct = prompt('Наименование товара:');
+    const quantityProduct = Number(prompt("Количество товара:"));
+    const categoryProduct = prompt('Категория товара:');
+    const countProduct = Number(prompt('Цена товара:'));
+    const summProduct = quantityProduct * countProduct;
+    console.log(`На складе ${quantityProduct} единицы товара "${nameProduct}" на сумму ${summProduct} деревянных`);
+}
