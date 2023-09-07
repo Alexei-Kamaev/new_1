a4af9fdd8afd12d4fa66bc774af9dbcf59b998ea
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-07 14:10:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+{
+    let nameProduct = 'Открытки';
+    let quantityProduct = 35;
+    const categoryProduct = 'Для праздника';
+    let countProduct = 350;
+
+    console.log('Наименование продукта: ', nameProduct);
+    console.log('Общая сумма товара: ', quantityProduct*countProduct, 'рублей');
+}
+
+{
+    let nameProduct = 'Кружки';
+    let quantityProduct = 25;
+    const categoryProduct = 'Для дома';
+    let countProduct = 450;
+
+    console.log('Наименование продукта: ', nameProduct);
+    console.log('Общая сумма товара: ', quantityProduct*countProduct, 'рублей');
+}
